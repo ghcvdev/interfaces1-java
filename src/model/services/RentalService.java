2f6a41fe1b337ed33160652b123ea12f59c2a04e
Gabriel de Carvalho <ghcvpro@gmail.com>
2023-12-24 18:56:34 -0300

    Solution with Interface

--- src/model/services/RentalService.java
+++ src/model/services/RentalService.java
@@ -6,74 +6,53 @@ import model.entities.CarRental;
 import model.entities.Invoice;
 
 public class RentalService {
-	
+
 	private Double pricePerHour;
 	private Double pricePerDay;
-	
-	private BrazilTaxService taxService;
-	
-	public RentalService(Double pricePerHour, Double pricePerDay, BrazilTaxService taxService) {
-		super();
+
+	private TaxService taxService;
+
+	public RentalService(Double pricePerHour, Double pricePerDay, TaxService taxService) {
 		this.pricePerHour = pricePerHour;
 		this.pricePerDay = pricePerDay;
 		this.taxService = taxService;
 	}
-	
-	
-	
+
 	public Double getPricePerHour() {
 		return pricePerHour;
 	}
-
-
 
 	public void setPricePerHour(Double pricePerHour) {
 		this.pricePerHour = pricePerHour;
 	}
 
-
-
 	public Double getPricePerDay() {
 		return pricePerDay;
 	}
-
-
 
 	public void setPricePerDay(Double pricePerDay) {
 		this.pricePerDay = pricePerDay;
 	}
 
-
-
-	public BrazilTaxService getTaxService() {
+	public TaxService getTaxService() {
 		return taxService;
 	}
 
-
-
-	public void setTaxService(BrazilTaxService taxService) {
-		this.taxService = taxService;
-	}
-
-
-
 	public void processInvoice(CarRental carRental) {
-		
+
 		double minutes = Duration.between(carRental.getStart(), carRental.getFinish()).toMinutes();
 		double hours = minutes / 60.0;
-		
+
 		double basicPayment;
 		if (hours <= 12.0) {
 			basicPayment = getPricePerHour() * Math.ceil(hours);
 		} else {
-			basicPayment = getPricePerDay() * Math.ceil(hours / 24.0); 
+			basicPayment = getPricePerDay() * Math.ceil(hours / 24.0);
 		}
-		
+
 		double tax = getTaxService().tax(basicPayment);
-		
+
 		carRental.setInvoice(new Invoice(basicPayment, tax));
 	}
-	
-	
 
 }
